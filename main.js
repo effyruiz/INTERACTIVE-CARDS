22c7c4f99fa083cdd765cdb2f34f06d35b6ea7f4
= <juanvidalsototovar@gmail.com>
2023-03-31 00:08:22 -0400

    on submit

--- main.js
+++ main.js
@@ -28,11 +28,58 @@ function changeCardValues(e) {
 // Atrapar formulario en el DOM
 const cardForm = document.querySelector('[data-form]');
 
-console.log('card', cardForm)
 // Agregar evento submit al formulario (formulario ha sido enviado)
 cardForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log('Formulario enviado', e.target);
+    // OBTENER DEL E TARGET EL FORMULARIO QUE SE ESTA ENVIANDO
+    const form = e.target;
+    // OBTENER TODOS LOS INPUTS QUE ESTAN EN EL FORMULARIO
+    const inputs = form.querySelectorAll('[data-input]');
+    // BANDERA/FLAG PARA SABER SI EL FORMULARIO TIENE UN ERROR
+    let formHasError = false;
+
+    // VERIFICAR CADA INPUT
+    inputs.forEach((i) => {
+        // VERIFICAR SI EL INPUT ESTA VACIO
+        if (!i.value) {
+            // SI LO ESTA, BUSCAR AL PADRE CON (CLOSEST) INPUT-WRAPPER Y BUSCAR DENTRO DE EL, EL COMENTARIO DE ERROR Y QUITARLE LA CLASE QUE LO OCULTA
+            i.closest('.input-wrapper').querySelector('.error').classList.remove('d-none');
+            // ACTIVAR LA BANDERA/FLAG DE ERROR
+            formHasError = true;
+        // VERIFICAR SI EL INPUT SOLO DEBERIA TENER NUMEROS
+        } else if (i.hasAttribute('data-only-number')) {
+            // USAR UNA BANDERA PARA VER SI EL INPUT TIENE SOLO NUMEROS (BUSCAR: REGEX)
+            const isNumber = /^\d+$/.test(i.value);
+            // SI EL INPUT TIENE LETRAS
+            if (!isNumber) {
+                // SI TIENE, BUSCAR AL PADRE CON CLOSEST Y BUSCAR DENTRO EL MENSAJE DE ERROR PARA NUMEROS Y BORRAR LA CLASE QUE LO OCULTA
+                i.closest('.input-wrapper').querySelector('.error-format').classList.remove('d-none');
+                // ACTIVAR LA BANDERA/FLAG DE ERROR
+                formHasError = true;
+            }
+        }
+    });
+
+    // SI EL FORMULARIO NO TIENE ERRORES MOSTRAR EL MENSAJE DE EXITO
+    if (!formHasError) {
+        continueMenu();
+    }
 });
+
+// 1. OBTENER EL DOM ELEMENT DEL BOTON, LA LANDING PAGE Y EL MENU COMPLETED
+const btnContinue = document.querySelector('[data-submit-button]');
+const addedCard = document.querySelector('#input-flex-completed');
+const landingPage = document.querySelector('.input-flex');
+
+// 2. AGREGAR UNA CLASE PARA OCULTAR EL MENU COMPLETED CUANDO CARGA LA PAGINA
+addedCard.classList.add('hide');
+
+// 4. DESPUES DE HACE CLICK AL BOTON SE DEBERIA MOSTRAR/OCULTAR EL MENU DE COMPLETADO
+function continueMenu() {
+    // ocultar landing page
+    landingPage.classList.add('hide');
+    // mostrar texto de exito
+    addedCard.classList.remove('hide');
+}
 
 
